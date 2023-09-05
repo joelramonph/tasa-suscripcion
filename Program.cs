c3f6b3857aa40a0379815b97dd49c32978a37226
Joel Parra <joelramonph@gmail.com>
2023-09-05 12:46:57 -0400

    estructuras if-elseif-else

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// here code
+if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\n Renew now and save 10%!");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!\n Renew now and save 20%!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration == 0)
+{
+    Console.Write("Your subscription has expired.");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
